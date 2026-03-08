2f47f64707882029a86586310ff14b6dc3e51e8f
aleios <dvsaleios@gmail.com>
2026-03-08 22:07:19 +1000

    feat: Script editor zoom via ctrl+scroll

--- tools/def_editor/widgets/luaeditor.py
+++ tools/def_editor/widgets/luaeditor.py
@@ -1,3 +1,4 @@
+from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QFontMetricsF
 from PyQt6.QtWidgets import QPlainTextEdit
 
@@ -11,4 +12,20 @@ class LuaEditor(QPlainTextEdit):
 
         space_width = QFontMetricsF(self.font()).horizontalAdvance(' ')
         self.setTabStopDistance(4.0 * space_width)
+
+    def wheelEvent(self, e):
+        if e.modifiers() & Qt.KeyboardModifier.ControlModifier:
+            zoom_dir = e.angleDelta().y()
+            font = self.font()
+            if zoom_dir > 0:
+                font_current = font.pointSize()
+                font.setPointSize(font_current + 1)
+            elif zoom_dir < 0:
+                font_current = font.pointSize()
+                font.setPointSize(font_current - 1)
+
+            self.setFont(font)
+            space_width = QFontMetricsF(font).horizontalAdvance(' ')
+            self.setTabStopDistance(4.0 * space_width)
+
 
