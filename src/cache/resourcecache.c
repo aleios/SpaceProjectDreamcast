3c5ef8d6b14c39b67dabd3dfd07130181639c2c1
aleios <dvsaleios@gmail.com>
2026-03-16 21:05:14 +1000

    fix: Finally fix the resource cache, add resize capability. Need to play around with the resize amount and load factor. refactor: Consolidate entry find into single function.

--- src/cache/resourcecache.c
+++ src/cache/resourcecache.c
@@ -6,6 +6,75 @@
 #define FNV_PRIME  0x01000193
 #define FNV_OFFSET 0x811c9dc5
 #define CACHE_INITIAL_CAPACITY 32
+#define CACHE_LOAD_FACTOR 0.7f
+
+static uint32_t resourcecache_get_index(resourcecache_t* cache, uint32_t hash) {
+    return hash & (cache->capacity - 1);
+}
+
+static uint32_t resourcecache_get_hash(const char* key) {
+    // FNV-1a implementation
+    uint32_t hash = FNV_OFFSET;
+    for(int i = 0; i < strlen(key); ++i) {
+        hash ^= (uint8_t)key[i];
+        hash *= FNV_PRIME;
+    }
+    return hash;
+}
+
+static resourcecache_entry_t* resourcecache_find_entry(resourcecache_t* cache, const char* key)
+{
+    if (!cache || !cache->entries || !key)
+        return nullptr;
+
+    const uint32_t hash = resourcecache_get_hash(key);
+    uint32_t idx  = resourcecache_get_index(cache, hash);
+
+    for (;;) {
+        const auto entry = &cache->entries[idx];
+
+        if (!entry->key)
+            return entry;
+
+        if (strcmp(entry->key, key) == 0)
+            return entry;
+
+        idx++;
+        if (idx == cache->capacity)
+            idx = 0;
+    }
+}
+
+static void resourcecache_ensure(resourcecache_t* cache) {
+    if ((float)cache->total_entries / (float)cache->capacity > CACHE_LOAD_FACTOR) {
+
+        // Calc new capacity. Just doubling for now
+        const auto old_capacity = cache->capacity;
+        cache->capacity = cache->capacity * 2;
+        const auto old_entries = cache->entries;
+
+        // Setup new cache
+        cache->entries = calloc(cache->capacity, sizeof(resourcecache_entry_t));
+        cache->total_entries = 0;
+
+        // Rehash and reset entry count
+        for (int i = 0; i < old_capacity; ++i) {
+            const auto old_entry = &cache->entries[i];
+
+            if (!old_entry->key) {
+                continue;
+            }
+
+            const auto entry = resourcecache_find_entry(cache, old_entry->key);
+
+            entry->key = old_entry->key;
+            entry->data = old_entry->data;
+
+            cache->total_entries++;
+        }
+        free(old_entries);
+    }
+}
 
 void resourcecache_init(resourcecache_t* cache) {
     cache->total_entries = 0;
@@ -16,8 +85,10 @@ void resourcecache_init(resourcecache_t* cache) {
 void resourcecache_destroy(resourcecache_t* cache) {
 
     // Free the allocated keys.
-    for(uint32_t i = 0; i < cache->total_entries; ++i) {
-        free(cache->entries[i].key);
+    for(uint32_t i = 0; i < cache->capacity; ++i) {
+        if (cache->entries[i].key) {
+            free(cache->entries[i].key);
+        }
     }
 
     // Free the entries
@@ -26,100 +97,52 @@ void resourcecache_destroy(resourcecache_t* cache) {
     cache->total_entries = 0;
 }
 
-uint32_t resourcecache_get_hash(const char* key) {
-    // FNV-1a implementation
-    uint32_t hash = FNV_OFFSET;
-    for(int i = 0; i < strlen(key); ++i) {
-        hash ^= (uint8_t)key[i];
-        hash *= FNV_PRIME;
-    }
-    return hash;
-}
-
-uint32_t resourcecache_get_index(resourcecache_t* cache, uint32_t hash) {
-    return hash & (cache->capacity - 1);
-}
-
 bool resourcecache_exists(resourcecache_t* cache, const char* key) {
-
-    return false;
+    const auto entry = resourcecache_find_entry(cache, key);
+    return entry && entry->key != nullptr;
 }
 
 void* resourcecache_get(resourcecache_t* cache, const char* key) {
-    if (cache->capacity == 0) return NULL;
+    if (!cache || !cache->entries || !cache->capacity)
+        return nullptr;
 
-    uint32_t hash = resourcecache_get_hash(key);
-    uint32_t index = resourcecache_get_index(cache, hash);
-    uint32_t start_index = index;
+    const auto entry = resourcecache_find_entry(cache, key);
 
-    while (cache->entries[index].key != NULL) {
-        if (strcmp(cache->entries[index].key, key) == 0) {
-            return cache->entries[index].data;
-        }
-
-        index = (index + 1) % cache->capacity;
-        if (index == start_index)
-            break; // Wrapped around
-    }
-
-    return NULL;
+    return (entry->key) ? entry->data : nullptr;
 }
 
 resourcecache_entry_t* resourcecache_set(resourcecache_t* cache, const char* key, void* value) {
-    if(!key || !cache || !cache->entries) {
+    if(!cache || !cache->entries) {
         return nullptr;
     }
 
-    //TODO: Dedup
-    uint32_t hash = resourcecache_get_hash(key);
-    uint32_t idx = resourcecache_get_index(cache, hash);
+    resourcecache_ensure(cache);
+    const auto entry = resourcecache_find_entry(cache, key);
 
-    // Try find existing entry
-    while(cache->entries[idx].key != NULL) {
-        int cmp = strcmp(key, cache->entries[idx].key);
-        if(cmp == 0) {
-            cache->entries[idx].data = value;
-            return &cache->entries[idx];
-        }
-        idx++;
-        // Wrap around if necessary
-        if(idx >= cache->capacity) {
-            idx = 0;
-        }
+    if (entry->key == nullptr) {
+        entry->key = strdup(key);
+        cache->total_entries++;
     }
 
-    // No entry found, add it.
-    cache->entries[idx].key = strdup(key);
-    cache->entries[idx].data = value;
-    return &cache->entries[idx];
+    entry->data = value;
+    return entry;
 }
 
 resourcecache_entry_t* resourcecache_get_or_insert(resourcecache_t* cache, const char* key) {
 
-    if(SHZ_UNLIKELY(cache->capacity) == 0) {
+    if(cache->capacity == 0) {
         resourcecache_init(cache);
     }
 
-    uint32_t hash = resourcecache_get_hash(key);
-    uint32_t idx = resourcecache_get_index(cache, hash);
+    resourcecache_ensure(cache);
+    const auto entry = resourcecache_find_entry(cache, key);
 
-    // Check for entries
-    while(cache->entries[idx].key != nullptr) {
-
-        // Compare key for a match.
-        const int cmp = strcmp(key, cache->entries[idx].key);
-        if(cmp == 0) {
-            return &cache->entries[idx];
-        }
-
-        // Keep going otherwise.
-        idx++;
-        if(idx >= cache->capacity) {
-            idx = 0;
-        }
+    if (entry->key == nullptr) {
+        entry->key = strdup(key);
+        entry->data = nullptr;
+        cache->total_entries++;
     }
-
-    return resourcecache_set(cache, key, nullptr);
+    return entry;
 }
 
 bool resourcecache_release(resourcecache_t* cache, void* res) {
